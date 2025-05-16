49d35b8b76d49d847d0543b20b535e7b2e5c614d
Y-Kanekoo <kaneko.yuki2001@gmail.con>
2025-05-16 17:55:05 +0900

    5/16

--- src/main/java/com/example/controller/MemberController.java
+++ src/main/java/com/example/controller/MemberController.java
@@ -1,4 +1,19 @@
 package com.example.controller;
 
+import com.example.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("member")
 public class MemberController {
+
+    @Autowired
+    private MemberRepository repository;
+    @GetMapping("")
+    public String index(){
+        return "member";
+    }
 }
--- src/main/java/com/example/domain/Member.java
+++ src/main/java/com/example/domain/Member.java
@@ -1,4 +1,54 @@
 package com.example.domain;
 
 public class Member {
+
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private Integer memberId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Integer memberId) {
+        this.memberId = memberId;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", memberId=" + memberId +
+                '}';
+    }
 }
